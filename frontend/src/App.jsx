dd8809ea07c3c113fd25d7bd19fd205b956de747
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-11-07 22:02:35 +0300

    Ignore backend .env

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <button>
+            Login
+          </button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignedOut />
+      </SignedIn>
+
+      <UserButton />
+    </div>
   )
 }
 
