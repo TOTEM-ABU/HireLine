8927c6aa6b1225785352cd85528d9a73a103a20d
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-11-07 10:58:42 +0300

    Project started

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running");
+});
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is listening on port ${ENV.PORT}`);
+});
