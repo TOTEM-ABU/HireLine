0aa9bc275ca285b2dbb70753ec8b1eefea32960f
AbuCoder <abubakrabdujalilov09@gmail.com>
2025-11-08 13:35:55 +0300

    Some little changes 2.0

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import { serve } from "inngest/express";
 
-import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
+
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
